8f6cf69e3b3d28028a7329d24bdef58bc21fb20d
saraheig <sarah.composto@heig-vd.ch>
2019-02-13 11:55:18 +0100

    Modify validations to allow nill for scores

--- app/models/player.rb
+++ app/models/player.rb
@@ -8,11 +8,11 @@ class Player < ApplicationRecord
   validates_presence_of :email, :message => 'L\'e-mail du joueur doit être spécifié.'
   validates_length_of :email, :maximum => 50, :message => 'L\'e-mail du joueur doit avoir maximum 50 caractères.'
 
-  validates_numericality_of :score_geo, :less_than_or_equal_to => 32767, :message => 'Le score géomatique du joueur doit valoir maximum 32767.'
-  validates_numericality_of :score_geo, :greater_than_or_equal_to => 0, :message => 'Le score géomatique du joueur doit valoir minimum 0.'
+  validates_numericality_of :score_geo, :allow_nil => true, :less_than_or_equal_to => 32767, :message => 'Le score géomatique du joueur doit valoir maximum 32767.'
+  validates_numericality_of :score_geo, :allow_nil => true, :greater_than_or_equal_to => 0, :message => 'Le score géomatique du joueur doit valoir minimum 0.'
 
-  validates_numericality_of :score_gci, :less_than_or_equal_to => 32767, :message => 'Le score génie civil du joueur doit valoir maximum 32767.'
-  validates_numericality_of :score_gci, :greater_than_or_equal_to => 0, :message => 'Le score génie civil du joueur doit valoir minimum 0.'
+  validates_numericality_of :score_gci, :allow_nil => true, :less_than_or_equal_to => 32767, :message => 'Le score génie civil du joueur doit valoir maximum 32767.'
+  validates_numericality_of :score_gci, :allow_nil => true, :greater_than_or_equal_to => 0, :message => 'Le score génie civil du joueur doit valoir minimum 0.'
 
   # Fuction to remove spaces in the fields
   def strip_blanks
