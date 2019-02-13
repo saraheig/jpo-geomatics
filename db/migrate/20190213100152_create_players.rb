f087aad4a3e48ddac7967d8669aded3982815754
saraheig <sarah.composto@heig-vd.ch>
2019-02-13 11:10:56 +0100

    Modify and run migration

--- db/migrate/20190213100152_create_players.rb
+++ db/migrate/20190213100152_create_players.rb
@@ -1,13 +1,13 @@
 class CreatePlayers < ActiveRecord::Migration[5.2]
   def change
     create_table :players do |t|
-      t.string :pseudo
-      t.string :email
-      t.integer :score_geo
-      t.integer :score_gci
+      t.string :pseudo, limit: 25
+      t.string :email, limit: 50
+      t.integer :score_geo, limit: 2
+      t.integer :score_gci, limit: 2
 
       t.timestamps
     end
-    add_index :players, :pseudo
+    add_index :players, :pseudo, unique: true
   end
 end
