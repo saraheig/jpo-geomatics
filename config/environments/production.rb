c1b216f2802a8f0c6933eb11591d74a0fe9a905d
saraheig <sarah.composto@heig-vd.ch>
2019-02-15 18:04:51 +0100

    Modify action cable configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ENV['ECG_JPO_ACTION_CABLE_ALLOWED_REQUEST_ORIGINS']]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
