19467c8d70dee82f6d09f34b7b53a13aeecfa201
saraheig <sarah.composto@heig-vd.ch>
2019-02-15 17:51:25 +0100

    Implement PlayersChannel

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,14 +2,12 @@ class PlayersController < ApplicationController
   before_action :set_player, only: [:show, :edit, :update, :destroy]
 
   # GET /players
-  # GET /players.json
   def index
     keyword = params[:keyword]
     @players = Player.search(keyword)
   end
 
   # GET /players/1
-  # GET /players/1.json
   def show
   end
 
@@ -23,42 +21,37 @@ class PlayersController < ApplicationController
   end
 
   # POST /players
-  # POST /players.json
   def create
     @player = Player.new(player_params)
 
     respond_to do |format|
       if @player.save
+        # Connexion to the players channel
+        PlayersChannel.broadcast(@player)
+
         format.html { redirect_to @player, notice: 'Le joueur a été créé.' }
-        format.json { render :show, status: :created, location: @player }
       else
         format.html { render :new }
-        format.json { render json: @player.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /players/1
-  # PATCH/PUT /players/1.json
   def update
     respond_to do |format|
       if @player.update(player_params)
         format.html { redirect_to @player, notice: 'Le joueur a été mis à jour.' }
-        format.json { render :show, status: :ok, location: @player }
       else
         format.html { render :edit }
-        format.json { render json: @player.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /players/1
-  # DELETE /players/1.json
   def destroy
     @player.destroy
     respond_to do |format|
       format.html { redirect_to players_url, notice: 'Le joueur a été supprimé.' }
-      format.json { head :no_content }
     end
   end
 
--- app/views/players/_player.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! player, :id, :pseudo, :email, :score_geo, :score_gci, :created_at, :updated_at
-json.url player_url(player, format: :json)
