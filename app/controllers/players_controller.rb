a0c7a7a51f3c740c34a634a065d6cdcdc6221de5
saraheig <sarah.composto@heig-vd.ch>
2019-02-16 17:21:09 +0100

    Modify views and actions restrictions

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,6 @@
 class PlayersController < AuthenticateController
   before_action :set_player, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate!, except: [:index]
 
   # GET /players
   def index
