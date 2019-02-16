db46dae0d8b111bbf9211cb4e0dc7035cf6e4108
saraheig <sarah.composto@heig-vd.ch>
2019-02-16 16:07:40 +0100

    Generate Session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   resources :users
   resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
