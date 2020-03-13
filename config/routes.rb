bc04863fcb211abab992574de544942314cd748e
saraheig <sarah.composto@heig-vd.ch>
2020-03-13 15:52:57 +0100

    Modify files related to rails update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: redirect('/players')
 
   resources :players
   resource :session, only: %i[new create destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
